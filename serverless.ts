7a73af8f657c2da26d9c199d2970f5a78677f495
jinhyeong.ju <jujh97@hanmail.net>
2023-06-26 01:52:33 +0900

    api id env

--- serverless.ts
+++ serverless.ts
@@ -1,14 +1,14 @@
 import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: "younha-archive",
+  service: "test-service",
   frameworkVersion: "3",
   provider: {
     name: "aws",
     runtime: "nodejs18.x",
     region: "ap-northeast-2",
     apiGateway: {
-      restApiId: "fff3g1wgdl",
+      restApiId: process.env.AWS_API_GATEWAY_API_KEY_ID,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
